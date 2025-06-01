34a5ecb208922e2d3a277e629c2e2ca04990e7f8
sajjadahammed <sajjad08@protonmail.com>
2025-06-01 19:03:49 +0530

    feat: Update layout and home page with MindMend branding and chatbot integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Brain } from "lucide-react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,12 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         <div className="flex items-center gap-3 absolute top-3 left-3 font-sans">
+            <div className="w-10 h-10 bg-gradient-to-r from-blue-600 to-green-600 rounded-xl flex items-center justify-center">
+              <Brain className="w-6 h-6 text-white" />
+            </div>
+            <h1 className="text-2xl font-bold text-white">MindMend</h1>
+          </div>
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-2 bg-gray-950  items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Link href={'/text'} className="text-white border border-zinc-50 rounded-sm p-24">Text</Link>
-      <Link href={'/audio'} className="text-white">Audio</Link>
+    <div className="grid  bg-gray-950  items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+     <HuggingFaceChatbot/>
     </div>
   );
 }
